03d65b15e7e7096a2cc7acb90c92d58b4b8599be
Cedric Honnet <honnet@telecom-paristech.org>
2017-12-08 19:37:38 +0100

    Test with PPI pulse width measure + in-out pin clone.

--- ppi_test.ino
+++ ppi_test.ino
@@ -1,19 +1,38 @@
 #include "PPI.h"
 
+// This test copies to state of an input pin in an output...
+// ...and measures the widths of the pulses of the input.
+// To simplify the test, it uses Serial RX as the input.
+
+#define clone_pin 22
+
 void setup() {
+    Serial.begin(115200);
 
-    //bind a timer event to the toggle of the pin
-    //set an interval for the timer (in milliseconds)
-    PPI.setTimerInterval(500);
-    PPI.setOutputPin(LED_G); //select the output pin
-    PPI.setShortcut(TIMER, PIN_TOGGLE); //bind TIMER event to PIN_TOGGLE action
+    // copy input in output (opposite)
+    PPI.setInputPin(PIN_SERIAL_RX);
+    PPI.setOutputPin(clone_pin);
+    PPI.setShortcut(PIN_CHANGE, PIN_TOGGLE);
 
-    PPI.setOutputPin(LED_B); //select the output pin
-    PPI.setShortcut(TIMER, PIN_TOGGLE); //bind TIMER event to PIN_TOGGLE action
+    // measure pulse width
+    PPI.setInputPin(PIN_SERIAL_RX);
 
+    PPI.setShortcut(PIN_LOW, TIMER_CLEAR);
+    PPI.setShortcut(PIN_LOW, TIMER_START);
+
+    PPI.setShortcut(PIN_HIGH, TIMER_CAPTURE); // capture register 0 for now
+    PPI.setShortcut(PIN_HIGH, TIMER_STOP);
 }
 
 
 void loop() {
-    delay(1000);
+    // capture register 0
+    int cc = nrf_timer_cc_read(NRF_TIMER1, nrf_timer_cc_channel_t(0));
+
+    static int cc_bak = 0;
+    if (cc_bak != cc) {         // check if a new value came
+        Serial.println(cc/16.); // convert to microseconds
+        cc_bak = cc;
+    }
 }
+
