4502d9e8fe569fb36c6ff5c61282c482f4ee6430
Cedric Honnet <honnet@telecom-paristech.org>
2018-03-09 11:21:02 +0100

    Use #undef in pinout for clarity.

--- customPinout.h
+++ customPinout.h
@@ -7,6 +7,7 @@
 #define LED_R                (10)
 #define LED_G                (14)
 #define LED_B                (12)
+#undef  PIN_LED
 #define PIN_LED              LED_G
 #define LED_BUILTIN          PIN_LED
 #define BUILTIN_LED          PIN_LED
@@ -15,10 +16,14 @@
 #define BUTTON               (26)
 
 // Serial
+#undef PIN_SERIAL_RX
+#undef PIN_SERIAL_TX
 #define PIN_SERIAL_RX        (25) /* J4 - E */
 #define PIN_SERIAL_TX        (27) /* J4 - D */
 
 // I2C
+#undef PIN_WIRE_SDA
+#undef PIN_WIRE_SCL
 #define PIN_WIRE_SDA         (29)
 #define PIN_WIRE_SCL         (28)
 
